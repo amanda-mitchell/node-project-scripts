28e501a3006470820d09637163fc7623f2f65ef9
David Mitchell <dave@fallingcanbedeadly.com>
2020-05-24 16:44:58 -0700

    feat: Add installation script.

---
+++ scripts/install.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+for (const config of [
+  'commitlint.config',
+  'husky.config',
+  'lint-staged.config',
+  '.eslintrc',
+  'prettier.config',
+]) {
+  const content = `module.exports = require('@david-mitchell/node-project-scripts/${config}');\n`;
+  fs.writeFileSync(`${config}.js`, content);
+}
