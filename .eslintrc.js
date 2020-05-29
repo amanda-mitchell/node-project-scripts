5dbdc8813973d632ee8f40025f75dc3cbf05d932
David Mitchell <dave@fallingcanbedeadly.com>
2020-05-28 17:26:19 -0700

    fix: Use correct configuration for ecmaVersion and jest.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: 'eslint:recommended',
-  parserOptions: { ecmaVersion: 6 },
+  parserOptions: { ecmaVersion: 2018 },
   env: { node: true },
+  overrides: [{ files: ['**/__tests__/*.js'], env: { jest: true } }],
 };
