eec0c1641950df14937bfa4af751ba874f4da0ef
Amanda Mitchell <she@mandymitchell.me>
2020-11-14 11:10:28 -0800

    fix: Only issue public release when package.json is configured to do so.

--- release.config.js
+++ release.config.js
@@ -1,6 +1,26 @@
+const fs = require('fs');
+
+let issuePublicRelease = false;
+try {
+  const packageJson = JSON.parse(
+    fs.readFileSync('./package.json', { encoding: 'utf-8' })
+  );
+  if (
+    packageJson.publishConfig &&
+    packageJson.publishConfig.access === 'public'
+  ) {
+    issuePublicRelease = true;
+  }
+} catch (x) {
+  // Skip this check if we can't find or parse package.json.
+}
+
 module.exports = {
   branches: ['main'],
-  plugins: [
+};
+
+if (issuePublicRelease) {
+  module.exports.plugins = [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
@@ -8,5 +28,5 @@ module.exports = {
       { registries: { github: {}, public: {} } },
     ],
     '@semantic-release/github',
-  ],
-};
+  ];
+}
