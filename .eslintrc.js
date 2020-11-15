b2651aa17a5ece74d96f0fbf38b791ac9819a9c7
Amanda Mitchell <she@mandymitchell.me>
2020-11-14 18:11:00 -0800

    fix: Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: 'eslint:recommended',
   parserOptions: { ecmaVersion: 2018 },
-  env: { node: true },
+  env: { node: true, es6: true },
   overrides: [{ files: ['**/__tests__/*.js'], env: { jest: true } }],
 };
