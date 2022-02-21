7cb5b47cc274fa065861e79ba942ce5b79006b08
Amanda Mitchell <she@mandymitchell.me>
2022-02-21 11:17:07 -0800

    fix: Lint mjs and cjs files.

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
   '*.js': ['eslint --fix', 'prettier --write'],
+  '*.mjs': ['eslint --fix', 'prettier --write'],
+  '*.cjs': ['eslint --fix', 'prettier --write'],
   '*.jsx': ['eslint --fix', 'prettier --write'],
   '*.ts': ['eslint --fix', 'prettier --write'],
   '*.tsx': ['eslint --fix', 'prettier --write'],
