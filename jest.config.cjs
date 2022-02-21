e7720ce9c46c2cd08e2a4c1b890f35ce9652995e
Amanda Mitchell <she@mandymitchell.me>
2022-02-21 15:46:15 -0800

    fix: Find ts-jest preset even when installed in a subfolder of node-project-scripts.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,19 @@
+const path = require('path');
+
+function findTsJestPreset() {
+  let resolvedPath = require.resolve('ts-jest');
+
+  while (path.basename(resolvedPath) !== 'ts-jest') {
+    const parent = path.dirname(resolvedPath);
+    if (resolvedPath === parent) {
+      throw new Error('unable to find ts-jest parent folder.');
+    }
+    resolvedPath = parent;
+  }
+}
+
 module.exports = {
-  preset: 'ts-jest',
+  preset: findTsJestPreset(),
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['.*\\.d\\.ts$'],
 };
